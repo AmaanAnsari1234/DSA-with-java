67a688aefaa39520a2ad9a0663d2a586abe822b5
Ansari Mohd Amaan <mohdamaanansari56@gmail.com>
2026-03-10 22:39:53 +0530

    Create StackUsingQueues.java

---
+++ Queue/StackUsingQueues.java
@@ -0,0 +1,74 @@
+// 225. Implement Stack using Queues
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P26 {
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    // Constructor
+    public P26() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    // Push element x onto stack
+    public void push(int x) {
+        q1.add(x);
+    }
+
+    // Removes the element on top of the stack and returns it
+    public int pop() {
+        // Move all elements except last to q2
+        while (q1.size() > 1) {
+            q2.add(q1.remove());
+        }
+
+        // Last element is the stack top
+        int popped = q1.remove();
+
+        // Swap q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+        return popped;
+    }
+
+    // Returns the element on top of the stack
+    public int top() {
+        // Move all elements except last to q2
+        while (q1.size() > 1) {
+            q2.add(q1.remove());
+        }
+
+        int top = q1.remove();
+        q2.add(top); // put it back
+
+        // Swap q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+        return top;
+    }
+
+    // Returns true if stack is empty
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+
+    // -------- MAIN METHOD --------
+    public static void main(String[] args) {
+        P26 stack = new P26();
+
+        stack.push(1);
+        stack.push(2);
+
+        System.out.println(stack.top());    // Expected: 2
+        System.out.println(stack.pop());    // Expected: 2
+        System.out.println(stack.empty());  // Expected: false
+    }
+}
