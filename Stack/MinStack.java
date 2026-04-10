a52e51c011f778c16daa5e86ea652b64a938e47e
Ansari Mohd Amaan <mohdamaanansari56@gmail.com>
2026-04-10 23:56:53 +0530

    Update MinStack.java

--- Stack/MinStack.java
+++ Stack/MinStack.java
@@ -4,3 +4,53 @@ Pattern: Stack (Design)
 Time Complexity: O(1) for all operations
 Space Complexity: O(n)
 */
+import java.util.*;
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            stack.pop();
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+
+        System.out.println("Min: " + minStack.getMin()); // -3
+
+        minStack.pop();
+
+        System.out.println("Top: " + minStack.top());    // 0
+        System.out.println("Min: " + minStack.getMin()); // -2
+    }
+}
