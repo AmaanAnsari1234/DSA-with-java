b93956a234f77b2c1ed5a58281d1b77bf55fed22
Ansari Mohd Amaan <mohdamaanansari56@gmail.com>
2026-04-11 00:04:00 +0530

    Update LargestElement.java

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -15,7 +15,7 @@ public class P1
         list.add(5);
         System.out.println(list);
         list.sort((o1, o2) -> o1-o2);
-        System.out.println(list);
-        System.out.println("Largest Element in the array is "+list.getLast());
+        System.out.println(list);                                               // Before Sorting [3, 7, 2, 9, 5]
+        System.out.println("Largest Element in the array is "+list.getLast());  // After Sorting [2, 3, 5, 7, 9]
     }
 }
