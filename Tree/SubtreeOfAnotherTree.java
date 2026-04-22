0c7556b0a8f1109488303b7600cee2402ec17b98
Ansari Mohd Amaan <mohdamaanansari56@gmail.com>
2026-04-22 23:10:39 +0530

    Create SubtreeOfAnotherTree.java

---
+++ Tree/SubtreeOfAnotherTree.java
@@ -0,0 +1,7 @@
+/*
+LeetCode 572 - Subtree of Another Tree
+Pattern: Recursion (DFS + Tree Comparison)
+Time Complexity: O(n * m)
+Space Complexity: O(h)
+*/
+| 47 | 572. Subtree of Another Tree | Tree | Java | [View Code](./Tree/SubtreeOfAnotherTree.java) |
