46daec02df74efa4f7abfa021dd3242173b105e5
Ansari Mohd Amaan <mohdamaanansari56@gmail.com>
2026-05-06 17:07:58 +0530

    Update DesignHashSet.java

--- Hashing/DesignHashSet.java
+++ Hashing/DesignHashSet.java
@@ -4,3 +4,49 @@ Pattern: Hashing
 Time Complexity: O(1) average
 Space Complexity: O(n)
 */
+
+import java.util.*;
+
+class MyHashSet {
+
+    boolean[] set;
+
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+
+    // add key
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    // remove key
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    // check if key exists
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+
+public class DesignHashSet {
+
+    public static void main(String[] args) {
+
+        MyHashSet hashSet = new MyHashSet();
+
+        hashSet.add(1);
+        hashSet.add(2);
+
+        System.out.println(hashSet.contains(1)); // true
+        System.out.println(hashSet.contains(3)); // false
+
+        hashSet.add(3);
+        System.out.println(hashSet.contains(3)); // true
+
+        hashSet.remove(2);
+        System.out.println(hashSet.contains(2)); // false
+    }
+}
